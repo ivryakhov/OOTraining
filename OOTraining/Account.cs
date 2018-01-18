8daabab48dd776b2093f2da746ac38353490dee2
Ivan Ryakhov <ivryakhov@gmail.com>
2018-01-18 13:04:36 +0300

    Procedureal Account implementation

---
+++ OOTraining/Account.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOTraining
+{
+    class Account
+    {
+        public decimal Balance { get; private set; }
+
+        private bool IsVerified { get; set; }
+        private bool IsClosed { get; set; }
+        private bool IsFrozen { get; set; }
+
+        private Action OnUnfreeze { get; }
+
+        public Account(Action onUnfreeze)
+        {
+            this.OnUnfreeze = onUnfreeze;
+        }
+        
+        public void Deposit(decimal amount)
+        {
+            if (this.IsClosed)
+                return;
+            if (this.IsFrozen)
+            {
+                this.IsFrozen = false;
+                this.OnUnfreeze();
+            }
+            this.Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (!this.IsVerified)
+                return;
+            if (this.IsClosed)
+                return;
+            if (this.IsFrozen)
+            {
+                this.IsFrozen = false;
+                this.OnUnfreeze();
+            }
+            this.Balance -= amount;
+
+        }
+
+        public void HolderVerified()
+        {
+            this.IsVerified = true;
+        }
+
+        public void Close()
+        {
+            this.IsClosed = true;
+        }
+
+        public void Freeze()
+        {
+            if (this.IsClosed)
+                return;
+            if (!this.IsVerified)
+                return;
+            this.IsFrozen = true;
+        }
+    }
+}
