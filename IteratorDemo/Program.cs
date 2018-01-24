84813ee7b48e34518c147960e0f5c5b646198487
Ivan Ryakhov <ivryakhov@gmail.com>
2018-01-24 14:09:42 +0300

    For loop implementation of finding the cheapest painter

--- IteratorDemo/Program.cs
+++ IteratorDemo/Program.cs
@@ -8,8 +8,26 @@ namespace IteratorDemo
 {
     class Program
     {
+        private static IPainter FindCheapestPainter(double sqMeters,
+                                                    IEnumerable<IPainter> painters)
+        {
+            double bestPrice = 0;
+            IPainter cheapest = null;
+
+            foreach (IPainter painter in painters)
+            {
+                if (painter.IsAvailable)
+                {
+                    double price = painter.EstimateCompensation(sqMeters);
+                    if (cheapest == null || price < bestPrice)
+                        cheapest = painter;
+                }
+            }
+            return cheapest;
+        }
         static void Main(string[] args)
         {
+            
         }
     }
 }
