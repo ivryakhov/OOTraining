a02d94c2441a02146979b0bdf1cb7914ef534be0
Ivan Ryakhov <ivryakhov@gmail.com>
2018-01-26 13:06:10 +0300

    Add functions to calculate painters job cost

--- IteratorDemo/Program.cs
+++ IteratorDemo/Program.cs
@@ -16,6 +16,36 @@ namespace IteratorDemo
                     .Where(painter => painter.IsAvailable)
                     .WithMinimum(painter => painter.EstimateCompensation(sqMeters));
         }
+
+        private static IPainter FindFastestPainter(double sqMeters,
+                                                    IEnumerable<IPainter> painters)
+        {
+            return
+                painters
+                    .Where(painter => painter.IsAvailable)
+                    .WithMinimum(painter => painter.EstimateTimeToPaint(sqMeters));
+        }
+
+        private static void WorkTogether(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            TimeSpan time =
+                TimeSpan.FromHours(
+                    1 /
+                    painters
+                        .Where(painter => painter.IsAvailable)
+                        .Select(painter => 1 / painter.EstimateTimeToPaint(sqMeters).TotalHours)
+                        .Sum());
+
+            double cost =
+                painters
+                    .Where(painter => painter.IsAvailable)
+                    .Select(painter =>
+                        painter.EstimateCompensation(sqMeters) /
+                        painter.EstimateTimeToPaint(sqMeters).TotalHours *
+                        time.TotalHours)
+                    .Sum();
+        }
+
         static void Main(string[] args)
         {
             
