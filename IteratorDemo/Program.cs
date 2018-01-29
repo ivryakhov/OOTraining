7962d25fb8b47c9484472577cd447c0798daf045
Ivan Ryakhov <ivryakhov@gmail.com>
2018-01-29 13:48:39 +0300

    Introduce ProportionalPainter class

--- IteratorDemo/Program.cs
+++ IteratorDemo/Program.cs
@@ -26,13 +26,13 @@ namespace IteratorDemo
                     .WithMinimum(painter => painter.EstimateTimeToPaint(sqMeters));
         }
 
-        private static void WorkTogether(double sqMeters, IEnumerable<IPainter> painters)
+        private static IPainter WorkTogether(double sqMeters, IEnumerable<IPainter> painters)
         {
             TimeSpan time =
                 TimeSpan.FromHours(
                     1 /
                     painters
-                        .Where(painter => painter.IsAvailable)
+                        .Where(painter => painter.IsAvailable) 
                         .Select(painter => 1 / painter.EstimateTimeToPaint(sqMeters).TotalHours)
                         .Sum());
 
@@ -44,6 +44,11 @@ namespace IteratorDemo
                         painter.EstimateTimeToPaint(sqMeters).TotalHours *
                         time.TotalHours)
                     .Sum();
+            return new ProportionalPainter()
+            {
+                TimePerSqMeters = TimeSpan.FromHours(time.TotalHours / sqMeters),
+                DollarPerHour = cost / time.TotalHours
+            };
         }
 
         static void Main(string[] args)
---
+++ IteratorDemo/ProportionalPainter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorDemo
+{
+    class ProportionalPainter : IPainter
+    {
+        public TimeSpan TimePerSqMeters { get; set; }
+        public double DollarPerHour { get; set; }
+
+        public bool IsAvailable => true;
+        public TimeSpan EstimateTimeToPaint(double sqMeters) =>
+            TimeSpan.FromHours(this.TimePerSqMeters.TotalHours * sqMeters);
+
+        public double EstimateCompensation(double sqMeters) =>
+            this.EstimateTimeToPaint(sqMeters).TotalHours * this.DollarPerHour;
+    }
+}
