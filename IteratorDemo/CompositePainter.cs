9f46e7cc0c8f2622097a8dd7b2310c46a0b5463d
Ivan Ryakhov <ivryakhov@gmail.com>
2018-01-30 14:15:58 +0300

    Apply Composite pattern on collection of Painters

---
+++ IteratorDemo/CompositePainter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorDemo
+{
+    class CompositePainter<TPainter> : IPainter
+        where TPainter : IPainter
+    {
+        private IEnumerable<TPainter> Painters { get; }
+
+        private Func<double, IEnumerable<TPainter>, IPainter> Reduce { get; }
+
+        public bool IsAvailable =>
+            this.Painters
+                .Any(painter => painter.IsAvailable);
+
+        public CompositePainter(IEnumerable<TPainter> painters,
+                                Func<double, IEnumerable<TPainter>, IPainter> reduce)
+        {
+            this.Painters = painters.ToList();
+            this.Reduce = reduce;
+        }
+
+        public TimeSpan EstimateTimeToPaint(double sqMeters) =>
+            this.Reduce(sqMeters, this.Painters).EstimateTimeToPaint(sqMeters);
+
+        public double EstimateCompensation(double sqMeters) =>
+            this.Reduce(sqMeters, this.Painters).EstimateCompensation(sqMeters);
+    }
+}
--- IteratorDemo/Program.cs
+++ IteratorDemo/Program.cs
@@ -8,52 +8,13 @@ namespace IteratorDemo
 {
     class Program
     {
-        private static IPainter FindCheapestPainter(double sqMeters,
-                                                    IEnumerable<IPainter> painters)
-        {
-            return
-                painters
-                    .Where(painter => painter.IsAvailable)
-                    .WithMinimum(painter => painter.EstimateCompensation(sqMeters));
-        }
-
-        private static IPainter FindFastestPainter(double sqMeters,
-                                                    IEnumerable<IPainter> painters)
-        {
-            return
-                painters
-                    .Where(painter => painter.IsAvailable)
-                    .WithMinimum(painter => painter.EstimateTimeToPaint(sqMeters));
-        }
-
-        private static IPainter WorkTogether(double sqMeters, IEnumerable<IPainter> painters)
-        {
-            TimeSpan time =
-                TimeSpan.FromHours(
-                    1 /
-                    painters
-                        .Where(painter => painter.IsAvailable) 
-                        .Select(painter => 1 / painter.EstimateTimeToPaint(sqMeters).TotalHours)
-                        .Sum());
-
-            double cost =
-                painters
-                    .Where(painter => painter.IsAvailable)
-                    .Select(painter =>
-                        painter.EstimateCompensation(sqMeters) /
-                        painter.EstimateTimeToPaint(sqMeters).TotalHours *
-                        time.TotalHours)
-                    .Sum();
-            return new ProportionalPainter()
-            {
-                TimePerSqMeters = TimeSpan.FromHours(time.TotalHours / sqMeters),
-                DollarPerHour = cost / time.TotalHours
-            };
-        }
-
         static void Main(string[] args)
         {
-            
+            IEnumerable<ProportionalPainter> painters = new ProportionalPainter[10];
+
+            IPainter painter = CompositePainterFactories.CreateCheapestSelector(painters);
+
         }
     }
 }
+ 
